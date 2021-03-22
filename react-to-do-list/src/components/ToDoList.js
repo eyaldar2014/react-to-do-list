8c94a1f5073394ae603af2d289222f945203e0b0
Eyal Dar <eyaldar2014@gmail.com>
2021-03-22 12:32:06 +0200

    setup

---
+++ react-to-do-list/src/components/ToDoList.js
@@ -0,0 +1,26 @@
+import react from "react";
+
+
+class ToDoList extends react.Component {
+
+    constructor(props) {
+        super();
+
+        this.state = {}
+    }
+
+    render() {
+        // console.log('[render]')
+        // console.log(this.state)
+
+
+        return <>
+            {console.log('a')}
+
+        </>
+    }
+
+}
+
+
+export default ToDoList
